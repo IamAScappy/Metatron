a3f2ab3204706e2134942a5d93f888de845c107e
almazrafi <almazrafi@gmail.com>
2016-11-17 21:43:04 +0300

    Package Manager file added

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Metatron
+//
+//  Created by Almaz Ibragimov on 17.11.16.
+//  Copyright © 2016 Almazrafi. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Metatron"
+)
